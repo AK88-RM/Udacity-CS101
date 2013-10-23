5ee3f4fb3acc18dee5cec58f2c9088798d7a0a73
riedlblower <github@michaelsexton.com>
2013-10-23 22:39:19 +0100

    added first file (problem set 1 question 8)

---
+++ unit1/1.8.find2.py
@@ -0,0 +1,20 @@
+# Assume text is a variable that
+# holds a string. Write Python code
+# that prints out the position
+# of the second occurrence of 'zip'
+# in text, or -1 if it does not occur
+# at least twice.
+
+# For example,
+#text = 'all zip files are zipped' 
+# >>> 18
+# text = 'all zip files are compressed'
+# >>> -1
+
+text = "all zip files are zipped" 
+
+#ENTER CODE BELOW HERE
+
+i = text.find("zip")
+j = text.find("zip",i+1)
+print j
