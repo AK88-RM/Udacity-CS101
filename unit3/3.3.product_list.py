37e1a8484b0d73642a22d0bf7219362c7aaea496
riedlblower <github@michaelsexton.com>
2014-04-22 13:57:37 +0100

    Create 3.3.product_list.py
    # Define a procedure, product_list,
    # takes as input a list of numbers,
    # and returns a number that is
    # the result of multiplying all
    # those numbers together.

---
+++ unit3/3.3.product_list.py
@@ -0,0 +1,16 @@
+def product_list(list1):
+    total = 1
+    for element in list1:
+        total = total * element
+    return total
+
+
+
+#print product_list([9])
+#>>> 9
+
+#print product_list([1,2,3,4])
+#>>> 24
+
+#print product_list([])
+#>>> 1
