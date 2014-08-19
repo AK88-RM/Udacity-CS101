89535081c1bbfebf917234a52cbc6967dfa92efa
riedlblower <github@michaelsexton.com>
2014-08-19 13:43:32 +0100

    Create 3o2.1.antisymmetric_square.py
    # By Dimitris_GR from forums
    # Modify Problem Set 31's (Optional) Symmetric Square to return True 
    # if the given square is antisymmetric and False otherwise. 
    # An nxn square is called antisymmetric if A[i][j]=-A[j][i] 
    # for each i=0,1,...,n-1 and for each j=0,1,...,n-1.

---
+++ unit3_optional2/3o2.1.antisymmetric_square.py
@@ -0,0 +1,33 @@
+def antisymmetric(A):
+    i = 0
+    j = 0
+    for i in range (0,len(A)):
+        sum = A[i][j] + A[j][i]
+    if sum == 0:
+        return True
+    else:
+        return False
+
+    
+
+# Test Cases:
+
+print antisymmetric([[0, 1, 2], 
+                     [-1, 0, 3], 
+                     [-2, -3, 0]])   
+#>>> True
+
+print antisymmetric([[0, 0, 0],
+                     [0, 0, 0],
+                     [0, 0, 0]])
+#>>> True
+
+print antisymmetric([[0, 1, 2], 
+                     [-1, 0, -2], 
+                     [2, 2,  3]])
+#>>> False
+
+print antisymmetric([[1, 2, 5],
+                     [0, 1, -9],
+                     [0, 0, 1]])
+#>>> False
